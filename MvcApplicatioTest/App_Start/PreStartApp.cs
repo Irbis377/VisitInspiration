eda4fd4cbe304d8d540966cb8b53121a58a551c4
Irina <irbis377@gmail.com>
2019-09-13 18:50:32 +0300

    Add files via upload

---
+++ MvcApplicatioTest/App_Start/PreStartApp.cs
@@ -0,0 +1,23 @@
+﻿using MvcApplicatioTest.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebActivatorEx;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(PreStartApp), "Start")]
+namespace MvcApplicatioTest.App_Start
+{
+    public class PreStartApp
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Метод запускается один раз перед стартом приложения        
+        /// </summary>
+        public static void Start()
+        {
+            logger.Info("Application PreStart");
+        }
+    }
+}
